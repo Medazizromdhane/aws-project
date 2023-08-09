c80c5c880a027f83091ff7b445b38cc2a55ff3c6
Medazizromdhane <aziz.romdhane@etudiant-fst.utm.tn>
2023-08-09 15:42:59 +0100

    Converted Navbar TO full JSX and edited the Login/Register panel to display correctly

--- src/components/Authentification.jsx
+++ src/components/Authentification.jsx
@@ -1,138 +1,121 @@
-import React, { useState } from 'react';
-import { FaFacebook, FaLinkedin, FaGoogle, FaRegEnvelope, FaLock, FaUser } from 'react-icons/fa';
+import React from "react";
+import * as Components from './AuthComponents';
+import {FaFacebook, FaGoogle, FaLinkedin, FaLock, FaRegEnvelope, FaUser} from "react-icons/fa";
 
-const SignInForm = () => {
-  return (
-    <div className='flex flex-col items-center'>
-      <div className='text-left font-bold'>
-        <span className='text-green-500'>SAAS</span>ESTIMATION
-      </div>
-      <div className='py-10'>
-        <h2 className='text-3xl font-bold text-green-500 mb-2'>Sign in to Account</h2>
-        <div className='border-2 w-10 border-green-500 inline-block mb-2'></div>
-        <div className='flex justify-center my-2'>
-          <a href='#' className='border-2 border-gray-200 rounded-full px-3 mx-1'>
-            <FaFacebook className='text-sm' />
-          </a>
-          <a href='#' className='border-2 border-gray-200 rounded-full px-3 mx-1'>
-            <FaLinkedin className='text-sm' />
-          </a>
-          <a href='#' className='border-2 border-gray-200 rounded-full px-3 mx-1'>
-            <FaGoogle className='text-sm' />
-          </a>
-        </div>
-        <p className='text-gray-400 my-3'>or use your email account</p>
-        <div className='flex flex-col items-center'>
-          <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
-            <FaRegEnvelope className='text-gray-400 m-2' />
-            <input type='email' name='email' placeholder='Email' className='bg-gray-100 outline-none text-sm flex-1' />
-          </div>
-          <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
-            <FaLock className='text-gray-400 m-2' />
-            <input type='password' name='password' placeholder='Password' className='bg-gray-100 outline-none text-sm flex-1' />
-          </div>
-          <div className='flex justify-between w-64 mb-5'>
-            <label className='flex items-center text-xs'><input type='checkbox' name='remember' className='mr-1' />Remember me</label>
-            <a href='#' className='text-xs'>Forgot Password?</a>
-          </div>
-          <a href='#' className='border-2 border-green-500 text-green-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-green-500 hover:text-white'>Sign In</a>
-        </div>
-      </div>
-    </div>
-  );
-};
+function Authentification() {
+  const [signIn, toggle] = React.useState(true);
+  return(
+      <div className='flex items-center justify-center min-h-screen py-2'>
+      <Components.Container>
 
-const SignUpForm = () => {
-  return (
-    <div className='flex flex-col items-center'>
-      <div className='text-left font-bold'>
-        <span className='text-green-500'>SAAS</span>ESTIMATION
-      </div>
-      <div className='py-10'>
-        <h2 className='text-3xl font-bold text-green-500 mb-2'>Create Account</h2>
-        <div className='border-2 w-10 border-green-500 inline-block mb-2'></div>
-        <div className='flex justify-center my-2'>
-          <a href='#' className='border-2 border-gray-200 rounded-full px-3 mx-1'>
-            <FaFacebook className='text-sm' />
-          </a>
-          <a href='#' className='border-2 border-gray-200 rounded-full px-3 mx-1'>
-            <FaLinkedin className='text-sm' />
-          </a>
-          <a href='#' className='border-2 border-gray-200 rounded-full px-3 mx-1'>
-            <FaGoogle className='text-sm' />
-          </a>
-        </div>
-        <div className='flex flex-col items-center'>
-          <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
-            <FaUser className='text-gray-400 m-2' />
-            <input type='text' name='name' placeholder='Name' className='bg-gray-100 outline-none text-sm flex-1' />
-          </div>
-          <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
-            <FaRegEnvelope className='text-gray-400 m-2' />
-            <input type='email' name='email' placeholder='Email' className='bg-gray-100 outline-none text-sm flex-1' />
-          </div>
-          <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
-            <FaLock className='text-gray-400 m-2' />
-            <input type='password' name='password' placeholder='Password' className='bg-gray-100 outline-none text-sm flex-1' />
-          </div>
-          <a href='#' className='border-2 border-green-500 text-green-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-green-500 hover:text-white'>Sign Up</a>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const Authentification = () => {
-  const [isSignIn, setIsSignIn] = useState(true);
-
-  const handleToggle = () => {
-    setIsSignIn((prevState) => !prevState);
-  };
-
-  return (
-    <div className='flex items-center justify-center min-h-screen py-2'>
-      <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
-        <div className='bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl'>
-          {isSignIn ? (
-            <>
-              <SignInForm />
-              <div className='w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12'>
-                <h2 className='text-3xl font-bold mb-2'>Hello, Friend!</h2>
-                <div className='border-2 w-10 border-white inline-block mb-2'></div>
-                <p className='mb-10'>Fill up personal information and start journey with us.</p>
-                <a href='#' className='border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-green-500' onClick={handleToggle}>
-                  Sign Up
+        <Components.SignUpContainer>
+          <div className='flex flex-col items-center'>
+            <div className='text-left font-bold'>
+              <span className='text-green-500'>SAAS</span>ESTIMATION
+            </div>
+            <Components.Form>
+            <div className='py-10'>
+              <h2 className='text-3xl font-bold text-green-500 mb-2'>Create Account</h2>
+              <div className='border-2 w-10 border-green-500 inline-block mb-2'></div>
+              <div className='flex justify-center my-2'>
+                <a href='#' className='border-2 border-gray-200 rounded-full px-3 mx-1'>
+                  <FaFacebook className='text-sm' />
+                </a>
+                <a href='#' className='border-2 border-gray-200 rounded-full px-3 mx-1'>
+                  <FaLinkedin className='text-sm' />
+                </a>
+                <a href='#' className='border-2 border-gray-200 rounded-full px-3 mx-1'>
+                  <FaGoogle className='text-sm' />
                 </a>
               </div>
-            </>
-          ) : (
-            <>
-              <div className='w-3/5 p-5'>
-                <SignUpForm />
+              <div className='flex flex-col items-center'>
+                <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
+                  <FaUser className='text-gray-400 m-2' />
+                  <input type='text' name='name' placeholder='Name' className='bg-gray-100 outline-none text-sm flex-1' />
+                </div>
+                <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
+                  <FaRegEnvelope className='text-gray-400 m-2' />
+                  <input type='email' name='email' placeholder='Email' className='bg-gray-100 outline-none text-sm flex-1' />
+                </div>
+                <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
+                  <FaLock className='text-gray-400 m-2' />
+                  <input type='password' name='password' placeholder='Password' className='bg-gray-100 outline-none text-sm flex-1' />
+                </div>
+                <a href='#' className='border-2 border-green-500 text-green-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-green-500 hover:text-white'>Sign Up</a>
               </div>
-              <div className='w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12'>
-                <h2 className='text-3xl font-bold mb-2'>Welcome Back!</h2>
-                <div className='border-2 w-10 border-white inline-block mb-2'></div>
-                <p className='mb-10'>To keep connected with us please login with your personal info.</p>
-                <a href='#' className='border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-green-500' onClick={handleToggle}>
-                  Sign In
+            </div>
+            </Components.Form>
+
+          </div>
+        </Components.SignUpContainer>
+
+        <Components.SignInContainer signinIn={signIn}>
+          <div className='flex flex-col items-center'>
+            <div className='text-left font-bold'>
+              <span className='text-green-500'>SAAS</span>ESTIMATION
+            </div>
+            <div className='py-10'>
+              <h2 className='text-3xl font-bold text-green-500 mb-2'>Sign in to Account</h2>
+              <div className='border-2 w-10 border-green-500 inline-block mb-2'></div>
+              <div className='flex justify-center my-2'>
+                <a href='#' className='border-2 border-gray-200 rounded-full px-3 mx-1'>
+                  <FaFacebook className='text-sm' />
+                </a>
+                <a href='#' className='border-2 border-gray-200 rounded-full px-3 mx-1'>
+                  <FaLinkedin className='text-sm' />
+                </a>
+                <a href='#' className='border-2 border-gray-200 rounded-full px-3 mx-1'>
+                  <FaGoogle className='text-sm' />
                 </a>
               </div>
-            </>
-          )}
-        </div>
-      </main>
-    </div>
-  );
-};
+              <p className='text-gray-400 my-3'>or use your email account</p>
+              <div className='flex flex-col items-center'>
+                <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
+                  <FaRegEnvelope className='text-gray-400 m-2' />
+                  <input type='email' name='email' placeholder='Email' className='bg-gray-100 outline-none text-sm flex-1' />
+                </div>
+                <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
+                  <FaLock className='text-gray-400 m-2' />
+                  <input type='password' name='password' placeholder='Password' className='bg-gray-100 outline-none text-sm flex-1' />
+                </div>
+                <div className='flex justify-between w-64 mb-5'>
+                  <label className='flex items-center text-xs'><input type='checkbox' name='remember' className='mr-1' />Remember me</label>
+                  <a href='#' className='text-xs'>Forgot Password?</a>
+                </div>
+                <a href='#' className='border-2 border-green-500 text-green-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-green-500 hover:text-white'>Sign In</a>
+              </div>
+            </div>
+          </div>
+        </Components.SignInContainer>
+
+        <Components.OverlayContainer signinIn={signIn}>
+          <Components.Overlay signinIn={signIn}>
+
+            <Components.LeftOverlayPanel signinIn={signIn}>
+              <Components.Title>Hello,Friend!</Components.Title>
+              <Components.Paragraph>
+                Enter your personal details and start journey with us
+              </Components.Paragraph>
+              <Components.GhostButton onClick={() => toggle(true)}>
+                Sign Up
+              </Components.GhostButton>
+            </Components.LeftOverlayPanel>
+
+            <Components.RightOverlayPanel signinIn={signIn}>
+              <Components.Title>Welcome Back!</Components.Title>
+              <Components.Paragraph>
+                To keep connected with us please login with your personal info
+              </Components.Paragraph>
+              <Components.GhostButton onClick={() => toggle(false)}>
+                Sign In
+              </Components.GhostButton>
+            </Components.RightOverlayPanel>
+
+          </Components.Overlay>
+        </Components.OverlayContainer>
+
+      </Components.Container></div>
+  )
+}
 
 export default Authentification;
-
-
-
-
-
-
-
-
-
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,6 @@ import { HashLink, NavHashLink } from "react-router-hash-link";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import styles from "../styles/Navbar.module.css";
-
-
 function Navbar() {
   const [connected, setConnected] = useState(false);
 
@@ -28,47 +26,146 @@ function Navbar() {
   const resize = () => {
     const liste = document.querySelector("." + styles.itemss);
     if (window.innerWidth > 670) {
-      liste.style.display = "";
+      if (liste) {
+        liste.style.display = "";
+      }
     }
   };
 
   const burgerDisplay = () => {
     const liste = document.querySelector("." + styles.itemss);
-    if (!liste.style.display) {
-      liste.style.display = "flex";
-    } else {
-      liste.style.display = "";
+    if (liste) {
+      if (!liste.style.display) {
+        liste.style.display = "flex";
+      } else {
+        liste.style.display = "";
+      }
     }
   };
 
-  document.onclick = function (e) {
+  const handleDocumentClick = (e) => {
     const liste = document.querySelector("#liste");
 
-    if (liste.style.display === "flex" && e.target.id !== "liste") {
-      liste.style.display = "";
-    } else if (liste.style.display === "" && e.target.id === "profilpic") {
-      liste.style.display = "flex";
-    } else if (liste.style.display === "flex" && e.target.id === "profilpic") {
-      liste.style.display = "";
+    if (liste && liste.style) {
+      if (liste.style.display === "flex" && e.target.id !== "liste") {
+        liste.style.display = "";
+      } else if (liste.style.display === "" && e.target.id === "profilpic") {
+        liste.style.display = "flex";
+      } else if (liste.style.display === "flex" && e.target.id === "profilpic") {
+        liste.style.display = "";
+      }
     }
   };
+
+  useEffect(() => {
+    document.addEventListener("click", handleDocumentClick);
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, []);
 
   const scrollWithOffset = (el, offset) => {
     const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
     const yOffset = offset;
     window.scrollTo({ top: yCoordinate + yOffset, behavior: "smooth" });
   };
-
   return (
-    <div className={styles.main}>
-      <header className={styles.container}>
-        <HashLink smooth to="/#">
-          <h1 className='text-3xl font-bold ml-4 sm:text-2xl'>SAAS</h1>
-          <h1 className='text-3xl font-bold mr-4 ml-10 sm:text-2xl'>Estimation</h1>
-        </HashLink>
+      <div className={styles.main}>
+        <header className={styles.container}>
+          <HashLink smooth to="/#">
+            <h1 className='text-3xl font-bold ml-4 sm:text-2xl text-green-500'>SAAS</h1>
+            <h1 className='text-3xl font-bold mr-4 ml-10 sm:text-2xl'>Estimation</h1>
+          </HashLink>
 
-
-        <nav className={styles.items}>
+          <nav className={styles.items}>
+            <NavHashLink className={styles.navBarItem} smooth to="/#">
+              Home
+            </NavHashLink>
+            <NavHashLink className={styles.navBarItem} smooth to="/servicespage">
+              Services
+            </NavHashLink>
+            <NavHashLink
+                className={styles.navBarItem}
+                smooth
+                to="/contact"
+                scroll={(e1) => scrollWithOffset(e1, 100)}
+            >
+              Contact Us
+            </NavHashLink>
+            <NavHashLink
+                className={styles.navBarItem}
+                smooth
+                to="/about"
+                scroll={(e1) => scrollWithOffset(e1, -160)}
+            >
+              About
+            </NavHashLink>
+          </nav>
+          <div className={styles.actions}>
+            <div className={styles.boss}>
+              <GrUser className={styles.profilePic} id="profilpic" />
+              <div className={styles.liste} id="liste">
+                {connected && (
+                    <div className={styles.listeLine}>
+                      <HashLink className={styles.ecriture} smooth to="/#">
+                        Notifications
+                      </HashLink>
+                      <GrNotification className={styles.icon} />
+                    </div>
+                )}
+                {connected && (
+                    <div className={styles.listeLine}>
+                      <a className={styles.ecriture} href="#">
+                        Profile
+                      </a>
+                      <GrUser className={styles.icon} />
+                    </div>
+                )}
+                {connected && (
+                    <div className={styles.listeLine}>
+                      <a className={styles.ecriture} href="#">
+                        Paramètres
+                      </a>
+                      <GrSettingsOption className={styles.icon} />
+                    </div>
+                )}
+                {connected ? (
+                    <div className={styles.listeLine}>
+                      <HashLink
+                          className={styles.ecriture}
+                          smooth
+                          to="/authentification"
+                          onClick={disconnect}
+                      >
+                        Se déconnecter
+                      </HashLink>
+                      <GrLogout className={styles.icon} />
+                    </div>
+                ) : (
+                    <div className={styles.listeLine}>
+                      <Link className={styles.ecriture} to="/authentification">
+                        Se Connecter
+                      </Link>
+                      <GrLogin className={styles.icon} />
+                    </div>
+                )}
+              </div>
+            </div>
+            <div className={styles.panier}>
+              <HashLink
+                  smooth
+                  to="/cart#cart"
+                  scroll={(e1) => scrollWithOffset(e1, -120)}
+              >
+                <FaShoppingBasket className={styles.icon} />
+              </HashLink>
+            </div>
+            <div className={styles.burger} onClick={burgerDisplay}>
+              <FaBars className={styles.icon} />
+            </div>
+          </div>
+        </header>
+        <nav className={styles.itemss}>
           <NavHashLink className={styles.navBarItem} smooth to="/#">
             Home
           </NavHashLink>
@@ -76,116 +173,24 @@ function Navbar() {
             Services
           </NavHashLink>
           <NavHashLink
-            className={styles.navBarItem}
-            smooth
-            to="/contact"
-            scroll={(e1) => scrollWithOffset(e1, 100)}
+              className={styles.navBarItem}
+              smooth
+              to="/contact"
+              scroll={(e1) => scrollWithOffset(e1, 100)}
           >
             Contact Us
           </NavHashLink>
           <NavHashLink
-            className={styles.navBarItem}
-            smooth
-            to="/about"
-            scroll={(e1) => scrollWithOffset(e1, -160)}
+              className={styles.navBarItem}
+              smooth
+              to="/about"
+              scroll={(e1) => scrollWithOffset(e1, -160)}
           >
             About
           </NavHashLink>
         </nav>
-        <div className={styles.actions}>
-          <div className={styles.boss}>
-            <GrUser className={styles.profilePic} id="profilpic" />
-            <div className={styles.liste} id="liste">
-              {connected && (
-                <div className={styles.listeLine}>
-                  <HashLink className={styles.ecriture} smooth to="/#">
-                    Notifications
-                  </HashLink>
-                  <GrNotification className={styles.icon} />
-                </div>
-              )}
-              {connected && (
-                <div className={styles.listeLine}>
-                  <a className={styles.ecriture} href="#">
-                    Profile
-                  </a>
-                  <GrUser className={styles.icon} />
-                </div>
-              )}
-              {connected && (
-                <div className={styles.listeLine}>
-                  <a className={styles.ecriture} href="#">
-                    Paramètres
-                  </a>
-                  <GrSettingsOption className={styles.icon} />
-                </div>
-              )}
-              {connected ? (
-                <div className={styles.listeLine}>
-                  <HashLink
-                    className={styles.ecriture}
-                    smooth
-                    to="/authentification"
-                    onClick={disconnect}
-                  >
-                    Se déconnecter
-                  </HashLink>
-                  <GrLogout className={styles.icon} />
-                </div>
-              ) : (
-                <div className={styles.listeLine}>
-                  <Link className={styles.ecriture} to="/authentification">
-                    Se Connecter
-                  </Link>
-                  <GrLogin className={styles.icon} />
-                </div>
-              )}
-            </div>
-          </div>
-          <div className={styles.panier}>
-            <HashLink
-              smooth
-              to="/cart#cart"
-              scroll={(e1) => scrollWithOffset(e1, -120)}
-            >
-              <FaShoppingBasket className={styles.icon} />
-            </HashLink>
-          </div>
-          <div className={styles.burger} onClick={burgerDisplay}>
-            <FaBars className={styles.icon} />
-          </div>
-        </div>
-      </header>
-      <nav className={styles.itemss}>
-        <NavHashLink className={styles.navBarItem} smooth to="/#">
-          Home
-        </NavHashLink>
-        <NavHashLink className={styles.navBarItem} smooth to="/servicespage">
-          Services
-        </NavHashLink>
-        <NavHashLink
-          className={styles.navBarItem}
-          smooth
-          to="/contact"
-          scroll={(e1) => scrollWithOffset(e1, 100)}
-        >
-          Contact Us
-        </NavHashLink>
-        <NavHashLink
-          className={styles.navBarItem}
-          smooth
-          to="/about"
-          scroll={(e1) => scrollWithOffset(e1, -160)}
-        >
-          About
-        </NavHashLink>
-      </nav>
-    </div>
+      </div>
   );
 }
 
 export default Navbar;
-
-
-
-
